69119625443116a67922c1fb755f42ac9f7ebd7d
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-09 08:07:32 +0000

    Create Circle.cs
    a circle class file

---
+++ AbstractClassesDemo/Circle.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+    public Circle(double radius) { Radius = radius; }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
