1afc7bcd486292eef0cc4f8b7c30b1cca8db5dbc
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-09 07:52:39 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        genericAnimal.MakeSound();
+        dog.MakeSound();
+        cat.MakeSound();
+
+        Console.ReadKey();
+    }
+}
