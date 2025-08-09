ee15849c6c21a584a9bed64e9364aa9e956a09e4
ESIANNAN <pannan027@st.ug.edu.gh>
2025-08-09 08:08:39 +0000

    Create Rectangle.cs
     a rectangle class file defining the methods

---
+++ AbstractClassesDemo/Rectangle.cs
@@ -0,0 +1,15 @@
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
